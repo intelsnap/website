6cbfc1279184aa30dd95d3f72b67ba301633cf20
Laeeq/18 <starsleo18@gmail.com>
2020-11-15 03:52:49 +0500

    home in process

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Row, Col } from 'antd'
 import Layout from '../components/layout'
-import hom from '../images/home.png'
+import hom from '../images/homet.png'
+import img from '../images/image3.jpeg'
 
 const Home = () => (
   <Layout>
@@ -19,19 +20,146 @@ const Home = () => (
     </Row>
     <Row>
       <Col span={3}> </Col>
-      <Col span={12}>
-        <h1 style={{ fontSize: '36px' }}> Starting Home </h1>
-        <p
-          style={{
-            textAlign: 'justify',
-            paddingTop: 'none',
-            textTransform: 'capitalize',
-            fontSize: '35px',
-          }}
+      <Col span={5}>
+        <h1
+          style={{ fontSize: '26px', textAlign: 'center', marginTop: '50px' }}
         >
           {' '}
-          Your partner in a fast-paced, digital world
-        </p>
+          Consulting{' '}
+        </h1>
+        <img
+          src={img}
+          alt="Consulting"
+          width=" 70%"
+          height="30%%"
+          style={{
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        />
+        <Row>
+          <p
+            style={{
+              textAlign: 'justify',
+              marginLeft: '45px',
+              marginRight: '40px',
+              paddingTop: '15px',
+            }}
+          >
+            {' '}
+            Leverage our expert knowledge in ...
+          </p>
+        </Row>
+        <Row style={{ marginTop: '67px' }}>
+          <p
+            style={{
+              fontWeight: 'bold',
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              paddingTop: '15px',
+            }}
+          >
+            READ MORE
+          </p>
+        </Row>
+      </Col>
+      <Col span={2}> </Col>
+      <Col span={5}>
+        <h1
+          style={{ fontSize: '26px', textAlign: 'center', marginTop: '50px' }}
+        >
+          {' '}
+          Digital Transformation{' '}
+        </h1>
+        <img
+          src={img}
+          alt="Consulting"
+          width=" 70%"
+          height="30%%"
+          style={{
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        />
+        <Row>
+          <p
+            style={{
+              textAlign: 'center',
+              display: 'block',
+              marginLeft: '45px',
+              marginRight: '50px',
+              paddingTop: '15px',
+            }}
+          >
+            {' '}
+            Collaborate with us to craft your strategic roadmap for innovation
+            with digital design technology.
+          </p>
+        </Row>
+        <Row style={{ marginTop: '20px' }}>
+          <p
+            style={{
+              fontWeight: 'bold',
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              paddingTop: '15px',
+            }}
+          >
+            READ MORE
+          </p>
+        </Row>
+      </Col>
+      <Col span={2}> </Col>
+      <Col span={5}>
+        <h1
+          style={{ fontSize: '26px', textAlign: 'center', marginTop: '50px' }}
+        >
+          {' '}
+          Software Development{' '}
+        </h1>
+        <img
+          src={img}
+          alt="Consulting"
+          width=" 70%"
+          height="30%%"
+          style={{
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        />
+        <Row>
+          <p
+            style={{
+              textAlign: 'center',
+              display: 'block',
+              marginLeft: '45px',
+              marginRight: '50px',
+              paddingTop: '15px',
+            }}
+          >
+            {' '}
+            Work with us to develop tools that will accelerate your workflow
+            using automation and computational design.
+          </p>
+        </Row>
+        <Row>
+          <p
+            style={{
+              fontWeight: 'bold',
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              paddingTop: '15px',
+            }}
+          >
+            READ MORE
+          </p>
+        </Row>
       </Col>
     </Row>
   </Layout>
